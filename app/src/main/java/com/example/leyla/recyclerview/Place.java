1d7654de40ca74ed87e61eca0afc03730e80ab58
leyla <l.faghanidelshad@gmail.com>
2017-09-06 12:58:16 -0700

    add to git

---
+++ app/src/main/java/com/example/leyla/recyclerview/Place.java
@@ -0,0 +1,13 @@
+package com.example.leyla.recyclerview;
+import android.content.Context;
+
+
+public class Place {
+    public String name;
+    public String imageName;
+    public boolean isFav;
+    public int getImageResourceId(Context context)
+    {
+        return    context.getResources().getIdentifier(this.imageName, "drawable", context.getPackageName());
+    }
+}
